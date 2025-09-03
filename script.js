a827ac79c066dc2fe52295a8d40d30a1c5ea9f17
Roberto F. Rodrigues <robertofr_@hotmail.com>
2025-09-02 23:52:25 -0300

    Finalização do projeto e criação do arquivo README.MD

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 function toggleMode() {
   const html = document.documentElement
-
-  // if (html.classList.contains("light")) {
-  //   html.classList.remove("light")
-  // } else {
-  //   html.classList.add("light")
-  // }
-
   html.classList.toggle("light")
+
+  // Pegar a imagem
+  const img = document.querySelector("#profile img")
+  const alt = document.querySelector("#profile img")
+
+  // Substituição da imagem
+  if (html.classList.contains("light")) {
+    // se tiver ligth mode, adciona aimagem ligth
+    img.setAttribute("src", "./assets/avatar-light.png")
+    alt.setAttribute("alt", "Foto de Roberto Ligth")
+  } else {
+    // se não tiver ligth mode, mantem a imagem
+    img.setAttribute("src", "./assets/avatar.png")
+    alt.setAttribute("alt", "Foto de Roberto Dark")
+  }
 }
